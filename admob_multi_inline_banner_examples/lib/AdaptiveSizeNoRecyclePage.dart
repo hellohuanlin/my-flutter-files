51f7e6d4df8495da3788d5e9eef58a4a585e306e
Huan Lin <hellohuanlin@gmail.com>
2025-05-12 10:37:08 -0700

    add comments

--- admob_multi_inline_banner_examples/lib/AdaptiveSizeNoRecyclePage.dart
+++ admob_multi_inline_banner_examples/lib/AdaptiveSizeNoRecyclePage.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:collection/collection.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdaptiveSizeNoRecyclePage extends StatefulWidget {
@@ -13,15 +12,17 @@ class AdaptiveSizeNoRecyclePage extends StatefulWidget {
 
 class _AdaptiveSizeNoRecyclePageState extends State<AdaptiveSizeNoRecyclePage> {
 
-  List<BannerAd> _banners = [];
-  Map<BannerAd, AdSize> _bannerSizes = {};
+
+  // A list of all the banners created.
+  final List<BannerAd> _banners = [];
+  // Keep track of sizes of the banners (since they can be different sizes).
+  final Map<BannerAd, AdSize> _bannerSizes = {};
 
   BannerAd _createBannerAd() {
     final String bannerId = Platform.isAndroid
         ? 'ca-app-pub-3940256099942544/6300978111'
         : 'ca-app-pub-3940256099942544/2934735716';
     AdSize adSize = AdSize.getCurrentOrientationInlineAdaptiveBannerAdSize(320);
-    print("Create a new banner");
     final BannerAd bannerAd = BannerAd(
       adUnitId: bannerId,
       request: const AdRequest(),
@@ -30,6 +31,7 @@ class _AdaptiveSizeNoRecyclePageState extends State<AdaptiveSizeNoRecyclePage> {
         onAdLoaded: (Ad ad) async {
           BannerAd bannerAd = (ad as BannerAd);
           final AdSize? adSize = await bannerAd.getPlatformAdSize();
+          // When the banner size is updated, we want to rebuild.
           if (adSize != null && adSize != _bannerSizes[bannerAd]) {
             setState(() {
               _bannerSizes[bannerAd] = adSize;
@@ -44,8 +46,10 @@ class _AdaptiveSizeNoRecyclePageState extends State<AdaptiveSizeNoRecyclePage> {
 
   BannerAd _getBannerAd(int bannerPosition) {
     if (bannerPosition < _banners.length) {
+      // If we already created a banner for this position, just reuse it.
       return _banners[bannerPosition];
     } else {
+      // Otherwise, create a new banner.
       BannerAd bannerAd = _createBannerAd();
       _banners.add(bannerAd);
       return bannerAd;
@@ -57,17 +61,22 @@ class _AdaptiveSizeNoRecyclePageState extends State<AdaptiveSizeNoRecyclePage> {
     return Scaffold(
       appBar: AppBar(title: Text('Adaptive Size, No Recycle')),
       body: ListView.builder(
+        // Arbitrary example of 1000 items in the list.
         itemCount: 1000,
         itemBuilder: (BuildContext context, int index) {
           if (index % 3 == 0) {
+            // We show a banner every 3 rows (i.e. row index 0, 3, 6, 9, 12, etc will be banner row)
             BannerAd bannerAd = _getBannerAd(index ~/ 3);
             AdSize? adSize = _bannerSizes[bannerAd];
             if (adSize == null) {
+              // Null adSize means the banner's content is not fetched yet.
               return SizedBox(height: 50, child: Text("banner is loading"));
             } else {
+              // Now this banner is loaded with ad content and corresponding ad size.
               return SizedBox(width: adSize.width.toDouble(), height: adSize.height.toDouble(), child: AdWidget(ad: bannerAd));
             }
           } else {
+            // Show your regular non-ad content.
             return SizedBox(height: 200, child: ColoredBox(color: Colors.yellow));
           }
         },
